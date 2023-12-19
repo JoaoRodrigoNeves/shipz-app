93a72320a88afbcf10a426becd7117a4b4a19d36
Pedro Miguel Costa Rolo <2212551@my.ipleiria.pt>
2023-12-19 19:46:09 +0000

    dev

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto/ProjetoApplication.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto/ProjetoApplication.java
@@ -1,9 +1,8 @@
 package pt.ipleiria.estg.dei.ei.dae.projeto;
-
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class HelloApplication extends Application {
+public class ProjetoApplication extends Application {
 
 }
