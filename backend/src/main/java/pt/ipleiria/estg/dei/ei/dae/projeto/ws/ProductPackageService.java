e0c602ed1810f0e5ac3c09371c13a101b0567e7d
João Rodrigo Neves <joao.rodrigo.neves07@gmail.com>
2024-01-01 22:09:37 +0000

    [product-package] CRUD + add / remove product

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto/ws/ProductPackageService.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto/ws/ProductPackageService.java
@@ -10,11 +10,13 @@ import jakarta.ws.rs.core.SecurityContext;
 import pt.ipleiria.estg.dei.ei.dae.projeto.dtos.ProductDTO;
 import pt.ipleiria.estg.dei.ei.dae.projeto.dtos.ProductManufacterDTO;
 import pt.ipleiria.estg.dei.ei.dae.projeto.dtos.ProductPackageDTO;
+import pt.ipleiria.estg.dei.ei.dae.projeto.ejbs.ProductBean;
 import pt.ipleiria.estg.dei.ei.dae.projeto.ejbs.ProductManufacterBean;
 import pt.ipleiria.estg.dei.ei.dae.projeto.ejbs.ProductPackageBean;
 import pt.ipleiria.estg.dei.ei.dae.projeto.entities.Product;
 import pt.ipleiria.estg.dei.ei.dae.projeto.entities.ProductManufacter;
 import pt.ipleiria.estg.dei.ei.dae.projeto.entities.ProductPackage;
+import pt.ipleiria.estg.dei.ei.dae.projeto.exceptions.MyEntityExistsException;
 import pt.ipleiria.estg.dei.ei.dae.projeto.exceptions.MyEntityNotFoundException;
 import pt.ipleiria.estg.dei.ei.dae.projeto.security.Authenticated;
 
@@ -31,6 +33,9 @@ public class ProductPackageService {
     private SecurityContext securityContext;
     @EJB
     private ProductPackageBean productPackageBean;
+
+    @EJB
+    private ProductBean productBean;
 
     private ProductPackageDTO productPackageToDTO(ProductPackage productPackage) {
         ProductPackageDTO productPackageDTO = new ProductPackageDTO(
@@ -75,7 +80,7 @@ public class ProductPackageService {
                 product.getProductManufacter().getName()
         );
 
-        if(product.getClientOrder() != null){
+        if (product.getClientOrder() != null) {
             productDTO.setClientOrderCode(product.getClientOrder().getCode());
         }
         return productDTO;
@@ -86,6 +91,17 @@ public class ProductPackageService {
     }
 
     //TODO create a product-package
+    @POST
+    @Path("/")
+    public Response create(ProductPackageDTO productPackageDTO) throws MyEntityExistsException {
+        ProductPackage productPackage = productPackageBean.create(
+                productPackageDTO.getType(),
+                productPackageDTO.getMaterial(),
+                productPackageDTO.getStatus(),
+                productPackageDTO.getManufacturingDate()
+        );
+        return Response.status(Response.Status.OK).entity(productPackage).build();
+    }
 
     //TODO get product-package details
     @GET
@@ -94,9 +110,23 @@ public class ProductPackageService {
         ProductPackage productPackage = productPackageBean.find(code);
         return Response.status(Response.Status.OK).entity(productPackageToDTONoProducts(productPackage)).build();
     }
+
     //TODO update product-package
+    @PUT
+    @Path("/")
+    public Response update(ProductPackageDTO productPackageDTO) throws MyEntityNotFoundException {
+        productPackageBean.update(productPackageDTO.getCode(), productPackageDTO.getType(), productPackageDTO.getMaterial(), productPackageDTO.getStatus(), productPackageDTO.getManufacturingDate());
+        ProductPackage productPackage = productPackageBean.find(productPackageDTO.getCode());
+        return Response.status(Response.Status.OK).entity(productPackageToDTONoProducts(productPackage)).build();
+    }
 
     //TODO delete product-package
+    @DELETE
+    @Path("{code}")
+    public Response delete(@PathParam("code") long code) throws MyEntityNotFoundException {
+        ProductPackage productPackage = productPackageBean.delete(code);
+        return Response.status(Response.Status.OK).entity(productPackageToDTONoProducts(productPackage)).build();
+    }
 
     //TODO get all product-packages
     @GET
@@ -104,6 +134,7 @@ public class ProductPackageService {
     public List<ProductPackageDTO> getAll() {
         return productPackageToDTOsNoProducts(productPackageBean.getAll());
     }
+
     //TODO get products from product-package
     @GET
     @Path("{packageCode}/products")
@@ -113,7 +144,19 @@ public class ProductPackageService {
         return Response.ok(productDTOS).build();
     }
 
-    //TODO add product
+    //TODO add product to package
+    @POST
+    @Path("{code}/product/{productCode}")
+    public Response addProduct(@PathParam("code") long code, @PathParam("productCode") long productCode) throws MyEntityNotFoundException, MyEntityExistsException {
+        productBean.addProductToPackage(productCode, code);
+        return Response.status(Response.Status.OK).build();
+    }
 
-    //TODO remove product
+    //TODO remove product from package
+    @DELETE
+    @Path("{code}/product/{productCode}")
+    public Response removeProduct(@PathParam("code") long code, @PathParam("productCode") long productCode) throws MyEntityNotFoundException, MyEntityExistsException {
+        productBean.removeProductFromPackage(productCode, code);
+        return Response.status(Response.Status.OK).build();
+    }
 }
